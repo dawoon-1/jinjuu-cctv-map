45e693e4e34bf7f49b9ed50ed5668e658eff79b3
dawoon-1 <g5ohno5@naver.com>
2025-06-13 00:46:02 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # 데이터 불러오기
-data = pd.read_csv("jinju_cctv.csv")
+data = pd.read_csv("경상남도 진주시_CCTV위치정보_20250501.csv")
 
 # 지도 출력
 st.title("진주시 CCTV 지도")
