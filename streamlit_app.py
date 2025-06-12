1d333a623cbade8f15c018fece1a5178e34a267c
dawoon-1 <g5ohno5@naver.com>
2025-06-13 00:55:02 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,28 @@
+%%writefile app.py
 import streamlit as st
+from streamlit_folium import folium_static
+import folium
+from folium.plugins import MarkerCluster
 import pandas as pd
 
-# 데이터 불러오기
-data = pd.read_csv("경상남도 진주시_CCTV위치정보_20250501.csv")
 
-# 지도 출력
-st.title("진주시 CCTV 지도")
-st.map(data[['위도', '경도']])
+st.title("진주시 CCTV 현황")
+
+df = pd.read_csv("경상남도 진주시_CCTV위치정보_20250501.csv", encoding='euc-kr')
+
+st.dataframe(df, height=200)
+
+df[["lat","lon"]] = df[["위도","경도"]]
+
+m = folium.Map(location=[35.1799817, 128.1076213], zoom_start=13)
+
+marker_cluster = MarkerCluster().add_to(m)
+
+for idx, row in df.iterrows():
+    folium.Marker(
+        location=[row["lat"], row["lon"]],
+        popup=row["설치장소"],
+        icon=folium.Icon(color="blue", icon="info-sign"),
+    ).add_to(marker_cluster)
+
+folium_static(m)
